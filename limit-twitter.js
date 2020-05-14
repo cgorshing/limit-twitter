f24ab512d73b0037db8f8ec2dc14e1a380798ea0
Chad Gorshing <gorshing@gmail.com>
2020-05-13 21:45:43 -0500

    Have some basic things working, but events aren't when closing the tab

--- limit-twitter.js
+++ limit-twitter.js
@@ -1,24 +1,56 @@
+'use strict';
 
-let maxTime = 1 * 60 * 1000
+// let maxTime = 1 * 60 * 1000
+let maxTime = 10 * 1000
+
 let timeoutID = -1;
-
-let timeSpent = 0;
 
 /* generic error handler */
 function onError(error) {
   console.log(error);
 }
 
-let startedTime = Date.now();
-console.log(`startedTime=${startedTime}`);
-//browser.tabs API can only be used/available in background script and Browser action popup scripts
+// TODO WHEN YOU JUST CLOSE THE TAB, THESE EVENTS DON'T FIRE!!!!!!
+// FIX THAT NEXT ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-// TODO Need a timer that fires while it is *only* visible to
-// possibly redirect because the time has exceeded.
-// Possibly pop-up asking user what they want to do
+let startedTime = Date.now();
+
+let maxTimeExpired = () => {
+  storeTimeSpent();
+
+  // TODO Callback that time has expired
+  // Show dialog warning of redirect in 30 seconds
+  alert(`Hey there - you have gone passed your budgeted amount for time on "the twitters"`);
+
+  if (window.confirm(`Hey there - you have gone passed your budgeted amount for time on "the twitters".
+
+  Click OK to go to Pocket now, otherwise we'll give ya 10 seconds.`)) {
+    location.assign('https://app.getpocket.com/');
+  }
+  else {
+    setTimeout(() => {
+      location.assign('https://app.getpocket.com/');
+    }, 10000);
+  }
+}
+
+let timeRetreived = (timeSpent) => {
+  console.log(`time_spent retreived=${timeSpent}`);
+
+  if (timeSpent == undefined || timeSpent == null) timeSpent = 0;
+
+  console.log(`timeSpent=${timeSpent}`);
+  if (timeSpent >= maxTime) {
+    // console.log('Should be redirecting now, but you have it commented out');
+    location.assign('https://app.getpocket.com/');
+  }
+  else {
+    timeoutID = setTimeout(maxTimeExpired, maxTime - timeSpent);
+  }
+}
 
 let visibilityChangeEvent = (e) => {
-  console.log(`event fired ${e} ${startedTime}`);
+  // console.log(`event fired ${e} ${startedTime}`);
 
   //https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
   if (document.visibilityState === 'visible') {
@@ -28,66 +60,56 @@ let visibilityChangeEvent = (e) => {
     // TODO How will we handle changing crossing over midnight
     // TODO How will we handle chaning timezones?
 
-    if (timeSpent >= maxTime) {
-      location.assign('https://app.getpocket.com/');
-    }
-    else {
-      timeoutID = setTimeout(() => {
-        // TODO Callback that time has expired
-        // Show dialog warning of redirect in 30 seconds
-        alert(`Hey there - you have gone passed your budgeted amount for time on "the twitters"`);
+    let gettingItem = browser.storage.local.get({
+      on_date: today(),
+      time_spent: 0
+    });
 
-        if (window.confirm(`Hey there - you have gone passed your budgeted amount for time on "the twitters".
+    gettingItem.then((result) => {
+      timeRetreived(result.time_spent);
+    });
 
-        Click OK to go to Pocket now, otherwise we'll give ya 10 seconds.`)) {
-          location.assign('https://app.getpocket.com/');
-        }
-        else {
-          setTimeout(() => {
-            location.assign('https://app.getpocket.com/');
-          }, 10000);
-        }
-      }, maxTime - timeSpent);
-    }
   }
   else {
-    console.log(`NOT Visible! ${startedTime} ${startedTime != null}`);
-    if (startedTime != null) {
-      console.log(`ASDFASDFASDFASDFASF ${Date.now() - startedTime}`);
-      console.log(`time spent so far ${Date.now() - startedTime}`);
-      // TODO Added to the list
-      // TODO Stop the timer
-      if (timeoutID != -1) clearTimeout(timeoutID);
-    }
+    storeTimeSpent();
   }
 }
 document.addEventListener("visibilitychange", visibilityChangeEvent, true);
 
+let storeTimeSpent = () => {
+  // console.log(`NOT Visible! ${startedTime} ${startedTime != null}`);
+  if (startedTime != null) {
+    let gettingItem = browser.storage.local.get({
+      on_date: today(),
+      time_spent: 0
+    });
+
+    let timeSpent = 0;
+
+    gettingItem.then((result) => {
+      if (result.time_spent != undefined && result.time_spent != null) {
+        timeSpent = result.time_spent;
+      }
+
+      console.log(`store off=${timeSpent + (Date.now() - startedTime)}`);
+
+      browser.storage.local.set({
+        on_date: today(),
+        time_spent: timeSpent + (Date.now() - startedTime)
+      });
+
+      if (timeoutID != -1) clearTimeout(timeoutID);
+    });
+  }
+}
+
 function today() {
   let now = new Date(Date.now());
-  return "" + now.getFullYear() + (now.getMonth() + 1) + now.getDate();
+  let year = now.getFullYear();
+  let month = ("" + (now.getMonth() + 1)).padStart(2, '0');
+  let date = now.getDate();
+
+  return `${year}-${month}-${date}`;
 }
 
-function initialize() {
-  var gettingAllStorageItems = browser.storage.local.get(null);
-  gettingAllStorageItems.then((results) => {
-    var noteKeys = Object.keys(results);
-    for (let noteKey of noteKeys) {
-      var curValue = results[noteKey];
-      displayNote(noteKey,curValue);
-      alert(`noteKey=${noteKey} and curValue=${curValue}`);
-    }
-  }, onError);
-}
-function removeEntry() {
-  var removingNote = browser.storage.local.remove(delNote);
-    removingNote.then(() => {
-      displayNote(newTitle, newBody);
-    }, onError);
-}
-
-initialize();
-
-// browser.storage.local.clear();
 visibilityChangeEvent(null);
-console.log(`Loaded and we are here - just a sec1`);
